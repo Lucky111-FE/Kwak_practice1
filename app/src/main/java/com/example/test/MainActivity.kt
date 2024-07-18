567a8249fd6afcb8141447674b175000983bbd2b
곽대건 <eorjs7578@gamil.com>
2024-07-18 21:31:57 +0900

    0718_extra_signup

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.test
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +13,38 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.signup2)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.extra_info)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Spinner 초기화
+        setupSpinners()
+    }
+
+    private fun setupSpinners() {
+        val yearSpinner: Spinner = findViewById(R.id.yearSpinner)
+        val monthSpinner: Spinner = findViewById(R.id.monthSpinner)
+        val daySpinner: Spinner = findViewById(R.id.daySpinner)
+
+        // 년도 Spinner
+        val years = (1923..2023).toList()
+        val yearAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, years)
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        yearSpinner.adapter = yearAdapter
+
+        // 월 Spinner
+        val months = (1..12).toList()
+        val monthAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, months)
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        monthSpinner.adapter = monthAdapter
+
+        // 날짜 Spinner
+        val days = (1..31).toList()
+        val dayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, days)
+        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        daySpinner.adapter = dayAdapter
     }
 }
-
