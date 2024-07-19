0286d1c1457a8d9f4da346d404530fd2baf655b4
곽대건 <eorjs7578@gmail.com>
2024-07-19 14:43:19 +0900

    0719_signup_modify

---
+++ app/src/main/java/com/example/test/RegisterStartFragment.kt
@@ -0,0 +1,25 @@
+package com.example.test
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.test.databinding.FragmentRegisterStartBinding
+
+class RegisterStartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val binding = FragmentRegisterStartBinding.inflate(layoutInflater)
+
+        binding.button.setOnClickListener {
+
+        }
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/test/ViewPageAdapter/ViewPageAdapter.kt
+++ app/src/main/java/com/example/test/ViewPageAdapter/ViewPageAdapter.kt
@@ -3,21 +3,25 @@ package com.example.test.ViewPageAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.test.RegisterExtraInfoFragment
+import com.example.test.RegisterFinishFragment
 import com.example.test.RegisterProfileFragment
+import com.example.test.RegisterStartFragment
 
 // 3개의 화면을 구성한다고 가정
 // OneFragment, TwoFragment, ThreeFragment
 class ViewPageAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
 
     // 페이지 갯수 설정
-    override fun getItemCount(): Int = 3
+    override fun getItemCount(): Int = 4
 
     // 불러올 Fragment 정의
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> RegisterProfileFragment()
-            1 -> RegisterProfileFragment()
+            0 -> RegisterStartFragment()
+            1 -> RegisterExtraInfoFragment()
             2 -> RegisterProfileFragment()
+            3 -> RegisterFinishFragment()
             else -> throw IllegalArgumentException("Invalid position $position")
         }
     }
